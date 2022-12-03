236b6bfb8681e032cb865c7b5aa6ac6b8d64b7d9
blast <rodfval.fv@gmail.com>
2022-12-03 16:11:01 +0100

    added new voices

--- api/main.py
+++ api/main.py
@@ -544,10 +544,10 @@ class UtilsPopulateSentencesApi(Resource):
     return get_response_str(utils.populate_new_sentences(get_chatbot_by_id(chatid), 5, None, True, chatid))
 
 
-@nsutils.route('/delete/bytext/<string:text>/<string:chatid>')
-class UtilsDeleteByText(Resource):
-  def get (self, text: str, chatid: str):
-    return get_response_str(utils.delete_by_text('./config/' + get_chatbot_by_id(chatid).storage.database_uri[17:], text))
+#@nsutils.route('/delete/bytext/<string:text>/<string:chatid>')
+#class UtilsDeleteByText(Resource):
+#  def get (self, text: str, chatid: str):
+#    return get_response_str(utils.delete_by_text('./config/' + get_chatbot_by_id(chatid).storage.database_uri[17:], text))
 	
 
 @nsutils.route('/upload/trainfile/json')
@@ -591,7 +591,7 @@ class FakeYouGetVoicesByCatClass(Resource):
 
 
 @nsutils.route('/download/<string:filename>')
-class FakeYouGetVoicesByCatClass(Resource):
+class UtilsDownloadClass(Resource):
   def get(self, filename: str):
     try:
       filepath = TMP_DIR_DISCORD + '/' + filename
