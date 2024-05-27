c304b1e65fce943868dd15bac235c47c2db9bf51
blast <rodfval.fv@gmail.com>
2024-05-27 21:41:46 +0200

    telegram bot update

--- telegram/installdeps.sh
+++ telegram/installdeps.sh
@@ -1,7 +1,3 @@
 #/bin/sh
-export TMPDIR=/mnt/media/tmp
-mkdir -p $TMPDIR
-/usr/bin/python3 -m venv .venv
+/home/blast/.pyenv/versions/3.10.13/bin/python3 -m venv .venv
 source .venv/bin/activate; pip3 install -r requirements.txt
-
-rm -rf $TMPDIR/*
