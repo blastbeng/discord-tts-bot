3e432a8b675d85ba242a9e18ce70e46dc57690ae
blast <rodfval.fv@gmail.com>
2024-08-18 11:41:07 +0200

    aaa

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,5 +2,5 @@
 rm /app/config/download*
 rm /app/config/markov*
 rm /app/config/sentences*
-uwsgi --ini /app/uwsgi.ini --enable-threads --log-4xx --log-5xx --disable-logging
+uwsgi --ini /app/uwsgi.ini --enable-threads --py-call-uwsgi-fork-hooks --log-4xx --log-5xx --disable-logging
 #uwsgi --ini /app/uwsgi.ini --enable-threads
