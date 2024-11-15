ca423cc780840fd46e8ac229588b925227492a7d
blastbeng <rodfval.fv@gmail.com>
2024-11-15 02:09:54 +0100

    bugfix

--- client/main.py
+++ client/main.py
@@ -1163,35 +1163,36 @@ async def on_voice_state_update(member, before, after):
                     #if voice_client:
                     #    await voice_client.disconnect()
                     await connect_bot_by_voice_client(voice_client, after.channel, None, member=member)
+                    if not voice_client.is_connected():
+                        await voice_client.channel.connect()
+                        time.sleep(2)
+                    if voice_client is not None:
+                        if voice_client.is_playing():
+                            voice_client.stop()
+                        await direct_play(voice_client, "https://www.myinstants.com/media/sounds/buongiorno-salvini.mp3")
             elif before.channel is None and after.channel is not None:
                 voice_client = get_voice_client_by_guildid(client.voice_clients, member.guild.id)
-                await connect_bot_by_voice_client(voice_client, after.channel, None, member=member)
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        logging.error("%s %s %s", exc_type, fname, exc_tb.tb_lineno, exc_info=1)
-
-@client.event
-async def on_member_join(member):
-    try:
-        perms = member.channel.permissions_for(member.channel.guild.me)
-        if (perms.administrator or perms.speak):
-            voice_client = get_voice_client_by_guildid(client.voice_clients, member.guild.id)
-            if voice_client is not None and not voice_client.is_playing():
-                await direct_play(voice_client, "https://www.myinstants.com/media/sounds/buongiorno-salvini.mp3")
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        logging.error("%s %s %s", exc_type, fname, exc_tb.tb_lineno, exc_info=1)
-
-@client.event
-async def on_member_leave(member):
-    try:
-        perms = member.channel.permissions_for(member.channel.guild.me)
-        if (perms.administrator or perms.speak):
-            voice_client = get_voice_client_by_guildid(client.voice_clients, member.guild.id)
-            if voice_client is not None and not voice_client.is_playing():
-                await direct_play(voice_client, "https://www.myinstants.com/media/sounds/buonasera-salvini.mp3")
+                perms = after.channel.permissions_for(after.channel.guild.me)
+                if (perms.administrator or perms.speak):
+                    await connect_bot_by_voice_client(voice_client, after.channel, None, member=member)
+                    if not voice_client.is_connected():
+                        await voice_client.channel.connect()
+                        time.sleep(2)   
+                    if voice_client is not None:
+                        if voice_client.is_playing():
+                            voice_client.stop()
+                        await direct_play(voice_client, "https://www.myinstants.com/media/sounds/buongiorno-salvini.mp3")
+            elif after.channel is None and before.channel is not None:
+                perms = before.channel.permissions_for(before.channel.guild.me)
+                if (perms.administrator or perms.speak):
+                    await connect_bot_by_voice_client(voice_client, before.channel, None, member=member)
+                    if not voice_client.is_connected():
+                        await voice_client.channel.connect()
+                        time.sleep(2)
+                    if voice_client is not None:
+                        if voice_client.is_playing():
+                            voice_client.stop()
+                        await direct_play(voice_client, "https://www.myinstants.com/media/sounds/buonasera-salvini.mp3")
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
